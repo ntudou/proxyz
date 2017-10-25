9caa5473289a11f42ca99e54e692968791550057
ntudou <ylqqmd@gmail.com>
2017-10-25 09:50:25 +0800

    proxy channel

--- c/proxy_client.go
+++ c/proxy_client.go
@@ -9,54 +9,78 @@ import (
 )
 
 func eachConn(remote string, tc net.Conn) {
-	uc, err := net.Dial("tcp", remote)
+	uc, err := net.DialTimeout("tcp", remote, time.Second*6)
 	if err != nil {
 		log.Println("get remote conn :", err.Error())
-		if uc!=nil {
+		if uc != nil {
 			uc.Close()
 		}
 		return
 	}
-	go netCompress(tc, uc)
-	go netUnCompress(uc, tc)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	go netCompress(tc, uc, ch1,ch2)
+	go netUnCompress(uc, tc, ch2,ch1)
 }
 
-func netCompress(src, dst net.Conn) error {
+func netCompress(src, dst net.Conn, ch1,ch2 chan bool) error {
 	buf := make([]byte, 1024)
 	var err error
 	for {
-		nr, err := src.Read(buf)
-		if err != nil {
-			log.Println(err.Error())
+		select {
+		case <-ch1:
 			break
-		}
-		if nr > 0 {
-			_, err = dst.Write(buf[0:nr])
+		default:
+			src.SetReadDeadline(time.Now().Add(time.Second))
+			nr, err := src.Read(buf)
 			if err != nil {
+				if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+					continue
+				}
 				log.Println(err.Error())
 				break
 			}
+			if nr > 0 {
+				_, err = dst.Write(buf[0:nr])
+				if err != nil {
+					log.Println(err.Error())
+					break
+				}
+			}
 		}
 	}
+	ch2 <- true
+	close(ch1)
 	return err
 }
-func netUnCompress(src, dst net.Conn) error {
+func netUnCompress(src, dst net.Conn, ch1,ch2 chan bool) error {
 	buf := make([]byte, 1024)
 	var err error
 	for {
-		nr, err := src.Read(buf)
-		if err != nil {
-			log.Println(err.Error())
+		select {
+		case <-ch1:
 			break
-		}
-		if nr > 0 {
-			_, err = dst.Write(buf[0:nr])
+		default:
+			src.SetReadDeadline(time.Now().Add(time.Second))
+			nr, err := src.Read(buf)
 			if err != nil {
+				if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+					continue
+				}
 				log.Println(err.Error())
 				break
 			}
+			if nr > 0 {
+				_, err = dst.Write(buf[0:nr])
+				if err != nil {
+					log.Println(err.Error())
+					break
+				}
+			}
 		}
 	}
+	ch2 <- true
+	close(ch1)
 	return err
 }
 
@@ -84,7 +108,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 	defer l.Close()
-	var idx int64= 0
+	var idx int64 = 0
 	for {
 		tc, err := l.Accept()
 		if err != nil {
@@ -93,7 +117,7 @@ func main() {
 			continue
 		}
 
-		go eachConn(host+":"+strconv.FormatInt(port+idx,10), tc)
+		go eachConn(host+":"+strconv.FormatInt(port+idx, 10), tc)
 		idx++
 		if idx == port+pcount {
 			idx = 0
--- s/proxy_server.go
+++ s/proxy_server.go
@@ -9,7 +9,7 @@ import (
 )
 
 func eachConn(remote string, tc net.Conn) {
-	uc, err := net.Dial("tcp", remote)
+	uc, err := net.DialTimeout("tcp", remote, time.Second*6)
 	if err != nil {
 		log.Println("get remote conn :", err.Error())
 		if uc != nil {
@@ -17,46 +17,70 @@ func eachConn(remote string, tc net.Conn) {
 		}
 		return
 	}
-	go netCompress(uc, tc)
-	go netUnCompress(tc, uc)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	go netCompress(uc, tc, ch1, ch2)
+	go netUnCompress(tc, uc, ch2, ch1)
 }
 
-func netCompress(src, dst net.Conn) error {
+func netCompress(src, dst net.Conn, ch1, ch2 chan bool) error {
 	buf := make([]byte, 1024)
 	var err error
 	for {
-		nr, err := src.Read(buf)
-		if err != nil {
-			log.Println(err.Error())
+		select {
+		case <-ch1:
 			break
-		}
-		if nr > 0 {
-			_, err = dst.Write(buf[0:nr])
+		default:
+			src.SetReadDeadline(time.Now().Add(time.Second))
+			nr, err := src.Read(buf)
 			if err != nil {
+				if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+					continue
+				}
 				log.Println(err.Error())
 				break
 			}
+			if nr > 0 {
+				_, err = dst.Write(buf[0:nr])
+				if err != nil {
+					log.Println(err.Error())
+					break
+				}
+			}
 		}
 	}
+	ch2 <- true
+	close(ch1)
 	return err
 }
-func netUnCompress(src, dst net.Conn) error {
+func netUnCompress(src, dst net.Conn, ch1, ch2 chan bool) error {
 	buf := make([]byte, 1024)
 	var err error
 	for {
-		nr, err := src.Read(buf)
-		if err != nil {
-			log.Println(err.Error())
+		select {
+		case <-ch1:
 			break
-		}
-		if nr > 0 {
-			_, err = dst.Write(buf[0:nr])
+		default:
+			src.SetReadDeadline(time.Now().Add(time.Second))
+			nr, err := src.Read(buf)
 			if err != nil {
+				if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+					continue
+				}
 				log.Println(err.Error())
 				break
 			}
+			if nr > 0 {
+				_, err = dst.Write(buf[0:nr])
+				if err != nil {
+					log.Println(err.Error())
+					break
+				}
+			}
 		}
 	}
+	ch2 <- true
+	close(ch1)
 	return err
 }
 
